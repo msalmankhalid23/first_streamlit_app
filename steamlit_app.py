3fcc255ed3ecd2716e795bc248f99216c9494a55
Muhammad Salman Khalid <m.salmankhalid23@gmail.com>
2022-05-17 19:55:57 +0500

    Update steamlit_app.py
    typo error fixed

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 streamlit.header('My Breakfast')
 streamlit.text('Paratha + Omlate')
-streamlit.('Milkshake - banana/strawberry/Mango')
+streamlit.text('Milkshake - banana/strawberry/Mango')
