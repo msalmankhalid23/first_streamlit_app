e9660ca479d3cd586536236849d18141ca20b6dd
Muhammad Salman Khalid <m.salmankhalid23@gmail.com>
2022-05-17 20:48:49 +0500

    Update steamlit_app.py
    Debugging

--- steamlit_app.py
+++ steamlit_app.py
@@ -18,7 +18,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 streamlit.write("Printing fruits selected")
 streamlit.write(*fruits_selected)
 
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the Page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_selected)
