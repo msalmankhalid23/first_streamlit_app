63f0952dedad841c1eb1c7cacba2775782fcdc5f
Muhammad Salman Khalid <m.salmankhalid23@gmail.com>
2022-05-17 23:19:22 +0500

    Update steamlit_app.py
    Debugging

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,12 +11,14 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Lets put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #To debug issue
 streamlit.write("Printing fruits selected")
-streamlit.write(*fruits_selected)
+fruits_selected2 = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), ['Avocado', 'Strawberries'])
+streamlit.write(*fruits_selected2)
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
