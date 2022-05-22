474d9f7a6daeb1c5edbc140b341107740fa0f230
Muhammad Salman Khalid <m.salmankhalid23@gmail.com>
2022-05-22 22:18:58 +0500

    Update steamlit_app.py
    fruityvice Api integration

--- steamlit_app.py
+++ steamlit_app.py
@@ -23,3 +23,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the Page
 streamlit.dataframe(fruits_selected)
+
+#New Section to display FruityVice api response
+import requests 
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+streamlit.text(fruityvice_response)
